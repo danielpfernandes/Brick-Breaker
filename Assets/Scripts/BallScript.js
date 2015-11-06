5492c5dee3d108cf9169195fd6a44e2e531ea28d
danielpfernandes <daniel.paivafernandes@gmail.com>
2015-11-06 15:48:47 -0500

    Major improvements on scenes and added bricks prefabs
    Changed paddle color and pattern
    Changed ball collor and pattern
    Added bounciness to VerticalLimitColider
    Created BallScript to control the relation between the ball and paddle
    shooting when gaming starts
    Added Particle System effects
    Added Bricks prefabs
    Created script to destroy bricks
    Added music LuvStep 2011 Mini Mix by Zom-v, available in
    https://soundcloud.com/zomvemokitty/zom-v-luvstep-2011-minimixd

---
+++ Assets/Scripts/BallScript.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var paddle: Paddle;
+var paddleToBallDist : Vector3;
+
+var notClicked = true;
+
+function Start () {
+	paddleToBallDist = transform.position - paddle.transform.position;
+}
+
+function Update () {
+	if(notClicked) {
+		transform.position = paddleToBallDist + paddle.transform.position;
+	}
+	ShootBall ();
+}
+
+function ShootBall () {
+	if(Input.GetMouseButtonDown(0) && notClicked){
+		var ballBody = GetComponent.<Rigidbody2D>();
+			ballBody.velocity = new Vector2(2,10);
+			notClicked = false;
+	}
+}
---
+++ Assets/Scripts/Brick.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var maxHits : int;
+var timesHit : int;
+
+function Start () {
+	timesHit = 0;
+}
+
+function Update () {
+
+}
+
+function OnCollisionEnter2D(c : Collision2D) {
+	timesHit ++;
+	if (timesHit > 1) {
+		Destroy(this.gameObject);
+	}
+}
